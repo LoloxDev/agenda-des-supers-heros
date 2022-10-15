ca2506f97871d2b1405917c44c5567b0031f8464
Ludovic NAULOT <pctronique@free.fr>
2022-10-15 23:28:08 +0200

    debut lecture fichier

---
+++ fonctions/config.php
@@ -0,0 +1,13 @@
+<?php
+
+if (defined('FILE_COUNTER')) {
+    define("FILE_COUNTER", dirname(__FILE__) . '/../data/counter.txt');
+}
+
+if (defined('FILE_DATA_DEF')) {
+    define("FILE_DATA_DEF", dirname(__FILE__) . '/../data/data.txt');
+}
+
+if (defined('FILE_DATA_CODE')) {
+    define("FILE_DATA_CODE", dirname(__FILE__) . '/../data/dataCode.txt');
+}
---
+++ fonctions/dataFile.php
@@ -0,0 +1,66 @@
+<?php 
+
+include dirname(__FILE__) . '/config.php';
+
+function loadFile(?string $nameFile = null):array {
+    if(empty($nameFile)) {
+        $nameFile = DATA_FILE_DEF;
+    }
+    $section = file_get_contents($nameFile, true);
+    $data = array();
+    $tabAllDonneur = explode("\n", $section);
+    foreach ($tabAllDonneur as $value) {
+        $tabDonneur = explode(",", $value);
+        array_push($data, $tabDonneur);
+    }
+    return $data;
+}
+
+function saveLine(?string $timeCode, ?string $codeDoneur, bool $passe = false, ?string $nameFile = null) {
+    if(empty($nameFile)) {
+        $nameFile = FILE_DATA_CODE;
+    }
+    $section = file_get_contents($nameFile, true);
+    file_put_contents($nameFile, $section."\n".$timeCode . "," . $codeDoneur . "," .($passe?"true":"false"));
+}
+
+function listePassage(?string $nameFile = null) {
+    if(empty($nameFile)) {
+        $nameFile = FILE_DATA_CODE;
+    }
+    $section = file_get_contents($nameFile, true);
+    $data = array();
+    $tabAllDonneur = explode("\n", $section);
+    foreach ($tabAllDonneur as $value) {
+        $tabDonneur = explode(",", $value);
+        if($tabDonneur[2] == "false") {
+            array_push($data, $tabDonneur);
+        }
+        
+    }
+    return $data;
+}
+
+function donneurPasser(?string $timeCode, ?string $codeDoneur, ?string $nameFile = null) {
+    if(empty($nameFile)) {
+        $nameFile = FILE_DATA_CODE;
+    }
+    $section = file_get_contents($nameFile, true);
+    $data = array();
+    $tabAllDonneur = explode("\n", $section);
+    foreach ($tabAllDonneur as $value) {
+        $tabDonneur = explode(",", $value);
+        if($tabDonneur[2] == "false") {
+            array_push($data, $tabDonneur);
+        }
+        
+    }
+    file_put_contents($nameFile, "");
+    foreach ($data as $value) {
+        saveLine($value[0], $value[1], $value[2]=="true");
+    }
+}
+
+function clearFile() {
+
+}
