51545e819d680e585b86125653952dcaed290294
Victor Chavanne <victor.chavanne@tutanota.com>
2022-10-15 18:11:14 +0200

    enleve du temps pour les rendez vous speciaux

--- fonctions/code_generation.php
+++ fonctions/code_generation.php
@@ -1,16 +1,28 @@
 <?php
 function generate_code($heure, $type, $rdv) {
-    $result = $heure;
+    // Commence par noter l'heure du rendez vous
+    // enlever 8 ou 9 minutes si le rendez vous est special
+    if ($type == 1) {
+        $result = date('Hi', strtotime($heure. ' -9 minutes'));
+    } elseif ($type == 2) {
+        $result = date('Hi', strtotime($heure. ' -8 minutes'));
+    } else {
+        $result = $heure;
+    }
+    // Ajoute le code du type de rendez vous
     $result = $result.strval($type);
+    //Ajoute un code pour dire si le donneur a un rendez vous ou non
     if ($rdv == false) {
         $result = $result."2";
     } else {
         $result = $result."1";
     }
+    // Ajoute le numero du rendez vous de la journée pour les cas speciaux
     $counter = (int) file_get_contents("fonctions/counter.txt") + 1;
     $counter = str_pad($counter , 4 , "0" , STR_PAD_LEFT);
     file_put_contents("fonctions/counter.txt",$counter);
     $result = $result.$counter;
+
     return $result;
 }
 ?> 
